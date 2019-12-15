42f6ed51113c428d3974e41fe5dfc8c7579162c8
lidia <lchavezca@unprg.edu.pe>
2019-12-15 00:38:26 -0500

    agregar 12.iteracion.py

---
+++ chavez_cabrera/12.iteracion.py
@@ -0,0 +1,13 @@
+#02
+import os
+msg="¶ ♀ ☼ §"
+msg=(os.sys.argv[1])
+for letra in msg:
+    if(letra=="¶"):
+        print("feliz navidad")
+    if(letra=="♀ "):
+        print("alumnos")
+    if(letra=="☼"):
+        print("estadisticos")
+    if(letra=="§"):
+        print("paz y amor en sus hojares")
