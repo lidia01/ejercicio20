d696dd6655ea6d9ccc35460a87dc5979f81e8233
lidia <lchavezca@unprg.edu.pe>
2019-12-15 00:48:57 -0500

    agregar 15.iteracion.py

---
+++ chavez_cabrera/15.iteracion.py
@@ -0,0 +1,13 @@
+#05
+import os
+msg="e@ap"
+msg=(os.sys.argv[1])
+for letra in msg:
+    if(letra=="e"):
+        print("estadistica")
+    if(letra=="@"):
+        print("todos")
+    if(letra=="a"):
+        print("aprenden")
+    if(letra=="p"):
+        print("programar")
