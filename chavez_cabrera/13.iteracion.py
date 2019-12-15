225e1a54bf316093bd4cbc3ce1f5318826ae14e5
lidia <lchavezca@unprg.edu.pe>
2019-12-15 00:40:36 -0500

    agregar 11.iteracion.py

---
+++ chavez_cabrera/13.iteracion.py
@@ -0,0 +1,15 @@
+#03
+import os
+msg="$•♦☺♥"
+msg=(os.sys.argv[1])
+for letra in msg:
+    if(letra=="$"):
+        print("todos")
+    if(letra=="•"):
+        print("unidos")
+    if(letra=="♦"):
+        print("luchemos por ser")
+    if(letra=="☺"):
+        print("felices")
+    if(letra=="♥"):
+        print("con amor")
