7c1c626686f33757d4a68be934792c7ea767869c
lidia <lchavezca@unprg.edu.pe>
2019-12-15 00:59:46 -0500

    agregar lote.bat

---
+++ chavez_cabrera/lote.bat
@@ -0,0 +1,136 @@
+ingrese edad:18
+ingrese edad:100
+ingrese edad:22
+
+ingrese clave:hola
+
+1
+2
+3
+
+ingrese edad18
+ingrese edad33
+ingrese edad19
+
+escribe un numero positivo:8
+gracias por su colaboracion
+escribe un numero positivo:11
+gracias por su colaboracion
+escribe un numero positivo:13
+gracias por su colaboracion
+
+sumas pares: 20301
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+programa terminado
+
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+
+sumas impares: 10201
+
+-10
+-9
+-8
+-7
+-6
+-5
+-4
+-3
+-2
+-1
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+
+hola
+lidia
+como estas?
+responde
+
+
+feliz navidad
+estadisticos
+paz y amor en sus hojares
+
+
+todos
+unidos
+luchemos por ser
+felices
+con amor
+
+llamame
+nececito
+dinero
+
+estadistica
+todos
+aprenden
+programar
+
+
+suma: 20100
+
+
+como estas?
+como estas?
+como estas?
+como estas?
+como estas?
+como estas?
+como estas?
+como estas?
+como estas?
+como estas?
+
+
+holalidia
+holadiana
+holamaribel
+holaguadalupe
+
+
+5
+6
+7
+8
+26
+
+
+suma: 190
+
+
+
