6f48d94601097ca6fa500e951b1fb2847e827a76
lidia <lchavezca@unprg.edu.pe>
2019-12-15 00:35:36 -0500

    agregar 11.iteracion.py

---
+++ chavez_cabrera/11.iteracion.py
@@ -0,0 +1,13 @@
+#01
+import os
+msg="hl%$"
+msg=(os.sys.argv[1])
+for letra in msg:
+    if(letra=="h"):
+        print("hola")
+    if(letra=="l"):
+        print("lidia")
+    if(letra=="%"):
+        print("como estas?")
+    if(letra=="$"):
+        print("responde")
