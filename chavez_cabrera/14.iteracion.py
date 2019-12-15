47ae6c0c71fe2f398795a2509b1d800d3548d6f4
lidia <lchavezca@unprg.edu.pe>
2019-12-15 00:42:42 -0500

    agregar 14.iteracion.py

---
+++ chavez_cabrera/14.iteracion.py
@@ -0,0 +1,15 @@
+#04
+import os
+msg="0#u$"
+msg=(os.sys.argv[1])
+for letra in msg:
+    if(letra=="0"):
+        print("llamame")
+    if(letra=="#"):
+        print("nececito")
+    if(letra=="♦"):
+        print("u")
+    if(letra=="urgente"):
+        print("felices")
+    if(letra=="$"):
+        print("dinero")
