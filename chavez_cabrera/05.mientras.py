d9d9f188fb0de82dcbc4d7a0f92810bc4e0c0de3
lidia <lchavezca@unprg.edu.pe>
2019-12-15 00:06:34 -0500

    agregar 05.mientras.py

---
+++ chavez_cabrera/05.mientras.py
@@ -0,0 +1,7 @@
+#05
+numero=int(input("escribe un numero positivo:"))
+while numero < 0:
+    print("¡has escrito un numero negativo! intentelo de nuevo")
+    numero=int(input("escribe un numero positivo:"))
+#fin_while
+print("gracias por su colaboracion")
